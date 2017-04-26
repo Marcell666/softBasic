4f6087af6681199b35645ba955178d7af7a2a13b
Marcell666 <brucemarcellino@hotmail.com>
2017-04-26 20:14:28 -0300

    Update main.c
    atualizei a main

--- main.c
+++ main.c
@@ -3,21 +3,21 @@
 #include "conv_utf.h"
 
 int main(void){
-	/*
-	FILE *f;
-	unsigned char c;
+	FILE *f, *f2;
 	f = fopen("asd.txt", "rb");
+	f2 = fopen("asd3.txt", "wb");
 	
 	if(f==NULL){
 		printf("erro!");
 		exit(1);
 	}
-	
-	while(fscanf(f, "%c", &c)>0) printf("%x ", c);
+
+	utf8_16(f, f2);
+
 	fclose(f);
-	*/
+	fclose(f2);
 	printf("\n");
-	printf("%d", contaUns((char) 0xE1));
+	//printf("%d", contaUns((char) 0xE1));
 
 	printf("\n");
 	
